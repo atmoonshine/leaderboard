47ebc219824e7b3f4bbd3a092fa277b2da9f2f37
Justin Darner <justin.darner@gmail.com>
2020-03-05 19:16:37 -0600

    Changes to QA backend

--- src/app/core/leaderboard.service.ts
+++ src/app/core/leaderboard.service.ts
@@ -5,7 +5,7 @@ import { Game } from '../shared/models/game';
 @Injectable({ providedIn: 'root' })
 export class LeaderboardService {
     get(limit: number = 30) {
-        return this.http.get<Game[]>(`https://www.atgames.net/arcadenet/backend/d2d/arcade/v1/leaderboards?limit=${limit}`);
+        return this.http.get<Game[]>(`https://acnet-qa.atgames.net/backend/d2d/arcade/v2/leaderboards?limit=${limit}`);
     }
 
     constructor(private http: HttpClient) {}
